8ec4f968ad4b580846827d14b9200dbd4eab7042
Tijana <trifunovic.tijana98@gmail.com>
2021-03-16 15:29:01 +0100

    Pocetne postavke frontend-a,navbar,footer,header

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container,Row,Col } from 'react-bootstrap'
+function Footer() {
+    return (
+        
+            <footer>
+                <Container>
+                    <Row>
+                        <Col className="text-center py-3">
+                        Copyright &copy; Proshop by Tamara Tosic and Tijana Trifunovic 
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
+       
+    )
+}
+
+export default Footer
