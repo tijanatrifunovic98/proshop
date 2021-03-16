d436f264e9075ae909dd6390ec0c3332dcfc98af
tamaratosic98 <tosict98@gmail.com>
2021-03-16 15:58:54 +0100

    Dodavanje Product.js komponente, slike za proizvode i niz proizvoda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
     <div>
       <Header />
       <main className="py-3">
         <Container> 
+          <HomeScreen/>
           <h1>Welcome</h1>
         </Container>
        
