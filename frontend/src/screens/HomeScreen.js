652c7ed2f9589d274866a87a69a12a80a2b86c1e
tamaratosic98 <tosict98@gmail.com>
2021-04-03 15:37:09 +0200

    Napravljena komponenta Loader za ucitavanje proizvoda.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,26 +1,39 @@
 import React , {useState, useEffect}from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import { listProducts } from '../actions/productActions'
 import axios from 'axios'
 function HomeScreen() {
-    const [products, setProducts] = useState([])
+    //const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {error, loading, products} = productList
     useEffect(()=>{
-        async function fetchProducts(){
-            const { data } = await axios.get('/api/products/')
-            setProducts(data)
-        }
-        fetchProducts()
-    },[])
+        // async function fetchProducts(){
+        //     const { data } = await axios.get('/api/products/')
+        //     setProducts(data)
+        // }
+        // fetchProducts()
+        dispatch(listProducts()) //ucitavanje proizvoda u redux deo
+
+    },[dispatch])
+    //const products = []
     return (
         <div>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product}/>
-                    </Col>
+            {loading ? <Loader/>
+            : error ? <h3>{error}</h3>
+            :  
+                <Row>
+                    {products.map(product => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product}/>
+                        </Col>
                 ))}
-            </Row>
+                </Row>
+        }
         </div>
     )
 }
