cabd8ddf672e0eed9908231db547c8b0a2087e5f
tamaratosic98 <tosict98@gmail.com>
2021-04-03 16:00:16 +0200

    Ucitavanje pojedinacnog proizvoda.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 import { PRODUCT_LIST_REQUEST, 
         PRODUCT_LIST_SUCCESS, 
-        PRODUCT_LIST_FAIL } from '../constants/productConstants'
+        PRODUCT_LIST_FAIL,
+        PRODUCT_DETAILS_REQUEST,
+        PRODUCT_DETAILS_SUCCESS,
+        PRODUCT_DETAILS_FAIL,
+     } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
     //api poziv za pristup proizvodima
@@ -17,6 +21,29 @@ export const listProducts = () => async (dispatch) => {
     }catch(error){
         dispatch({
             type:PRODUCT_LIST_FAIL,
+            payload:error.response && error.response.data.message
+            ? error.response.data.message 
+            : error.message,
+        })
+    }
+
+}
+
+
+export const listProductDetails = (id) => async (dispatch) => {
+    //api poziv za pristup proizvodima
+    try{
+        dispatch({type:PRODUCT_DETAILS_REQUEST})
+
+        const { data } = await axios.get(`/api/products/${id}`)
+        dispatch({
+            type:PRODUCT_DETAILS_SUCCESS,
+            payload:data
+        })
+
+    }catch(error){
+        dispatch({
+            type:PRODUCT_DETAILS_FAIL,
             payload:error.response && error.response.data.message
             ? error.response.data.message 
             : error.message,
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -27,13 +27,13 @@ function HomeScreen() {
             {loading ? <Loader/>
             : error ? <Message variant='danger'>{error}</Message>
             :  
-                <Row>
+                (<Row>
                     {products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                             <Product product={product}/>
                         </Col>
                 ))}
-                </Row>
+                </Row>)
         }
         </div>
     )
