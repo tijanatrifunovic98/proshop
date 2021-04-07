801d857c4e08db198fd6977d0dcd352c3b068291
tamaratosic98 <tosict98@gmail.com>
2021-04-07 17:54:40 +0200

    Prijava i odjava korisnika sa sajta.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 function App() {
   return (
     <Router>
@@ -12,6 +13,7 @@ function App() {
       <main className="py-3">
         <Container> 
           <Route path='/' component={HomeScreen} exact/>
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/> 
         </Container>
