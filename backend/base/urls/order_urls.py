eb1c7d8f6266c253a4aec72ec5a02200b9d52338
tamaratosic98 <tosict98@gmail.com>
2021-04-10 12:44:38 +0200

    Backend postavke za porudzbinu, serijalizacija

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from base.views import order_views as views
 
 urlpatterns = [
-   
+    #pre-tekst je api/orders
+   path('add/', views.addOrderItems, name='orders-add'),
 ]
---
+++ backend/base/views/order_views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.models import Product, Order, OrderItem, ShippingAddress
+from base.serializers import ProductSerializer, OrderSerializer
+
+from rest_framework import status
+
+@api_view(['POST']) #post request jer saljemo podatke
+@permission_classes(['IsAuthenticated']) #bilo koji user koji je ulogovan
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail':'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        #pravljenje porudzbine
+        order = Order.objects.create(
+            user = user,
+            paymentMethod = data['paymentMethod'],
+            taxPrice = data['taxPrice'],
+            shippingPrice = data['shippingPrice'],
+            totalPrice = data['totalPrice']
+        )
+        #kreiranje shipping adrese
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
+        )
+        #prolazenje kroz stavke i postavljanje porudzbine na te stavke, pravljenje orderItem
+        for i in orderItems:
+            product = Product.objects.get(_id=i['product']) #product je na frontendu id
+            item  = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url,
+                
+            )
+            #updejtovanje kolicine proizvoda posle kupovine
+            product.countInStock -= item.qty
+            product.save()
+    serializer = OrderSerializer(order, many=True)
+    return Response(serializer.data)
