80bc87aa8db6153608013b09972f23e719d78b2e
tamaratosic98 <tosict98@gmail.com>
2021-04-10 11:54:32 +0200

    Payment stranica

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,8 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS} from '../constants/cartConstants'
+import { CART_ADD_ITEM, 
+        CART_REMOVE_ITEM, 
+        CART_SAVE_SHIPPING_ADDRESS, 
+        CART_SAVE_PAYMENT_METHOD
+} from '../constants/cartConstants'
 
 export const cartReducer = (state={cartItems:[], shippingAddress:{}},action) => {
     switch(action.type){
@@ -26,6 +30,11 @@ export const cartReducer = (state={cartItems:[], shippingAddress:{}},action) => 
             return {
                 ...state,
                 shippingAddress:action.payload //form data koji saljemo
+            }
+        case CART_SAVE_PAYMENT_METHOD:
+            return{
+                ...state,
+                paymentMethod:action.payload
             }
         default:
             return state
