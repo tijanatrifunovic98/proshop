5831e0334055ccdb033d7b5cc1d61aa92630eabc
tamaratosic98 <tosict98@gmail.com>
2021-04-13 16:03:57 +0200

    Brisanje user-a sa sajta iz admin menu dela

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { listUsers } from '../actions/userActions'
+import { listUsers, deleteUser } from '../actions/userActions'
 
 function UserListScreen({history}) {
     const dispatch = useDispatch()
@@ -14,6 +14,9 @@ function UserListScreen({history}) {
 
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
+
+    const userDelete = useSelector(state => state.userDelete)
+    const {success:successDelete} = userDelete
     
     useEffect(()=>{
         if(userInfo && userInfo.isAdmin){
@@ -24,10 +27,14 @@ function UserListScreen({history}) {
             history.push('/login')
         }
         dispatch(listUsers())
-    }, [dispatch, history])
+    }, [dispatch, history, successDelete])
 
     const deleteHandler = (id) => {
-        console.log('DELETE:', id)
+        //console.log('DELETE:', id)
+        if(window.confirm('Are you sure you want to delete this user?')){
+            dispatch(deleteUser(id))
+        }
+        
     }
 
     return (
