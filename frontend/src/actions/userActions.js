99f0493b7ec7c562000bbf8201cfb66743e5b226
tamaratosic98 <tosict98@gmail.com>
2021-04-13 15:09:22 +0200

    MyOrders api, tabela za prikaz svih porudzbina

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -21,7 +21,7 @@ import {
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_RESET,
  } from '../constants/userConstants'
-
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 export const login = (email, password) => async (dispatch) => {
     try{
         dispatch({
@@ -57,6 +57,7 @@ export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({type:USER_LOGOUT})
     dispatch({type:USER_DETAILS_RESET})
+    dispatch({type:ORDER_LIST_MY_RESET}) //kad se izloguje da i njegove porudzbine budu obrisane iz local storage
 }
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -127,6 +128,8 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
         })
     }
 }
+
+
 export const updateUserProfile = (user) => async (dispatch, getState) => {
     try{
         dispatch({
